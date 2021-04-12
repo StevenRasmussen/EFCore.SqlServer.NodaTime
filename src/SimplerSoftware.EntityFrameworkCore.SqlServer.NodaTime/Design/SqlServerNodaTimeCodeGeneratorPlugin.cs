a4b52940b34b3d0321a1623f43ec0061891e240e
Steven Rasmussen <steven.rasmussen@gmail.com>
2021-04-12 09:43:05 -0600

    Add design time support.

---
+++ src/SimplerSoftware.EntityFrameworkCore.SqlServer.NodaTime/Design/SqlServerNodaTimeCodeGeneratorPlugin.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Scaffolding;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SimplerSoftware.EntityFrameworkCore.SqlServer.NodaTime.Design
+{
+    public class SqlServerNodaTimeCodeGeneratorPlugin : ProviderCodeGeneratorPlugin
+    {
+        public override MethodCallCodeFragment GenerateProviderOptions() 
+            => new MethodCallCodeFragment(nameof(ServiceCollectionExtensions.AddNodaTime));
+    }
+}
---
+++ src/SimplerSoftware.EntityFrameworkCore.SqlServer.NodaTime/Design/SqlServerNodaTimeDesignTimeServices.cs
@@ -0,0 +1,23 @@
+﻿using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Scaffolding;
+using Microsoft.EntityFrameworkCore.SqlServer.Storage;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SimplerSoftware.EntityFrameworkCore.SqlServer.NodaTime.Design
+{
+    [UsedImplicitly]
+    public class SqlServerNodaTimeDesignTimeServices : IDesignTimeServices
+    {
+        public void ConfigureDesignTimeServices(IServiceCollection serviceCollection)
+            => serviceCollection
+                .AddSingleton<IRelationalTypeMappingSourcePlugin, InstantTypeMappingSourcePlugin>()
+                .AddSingleton<IRelationalTypeMappingSourcePlugin, LocalDateTypeMappingSourcePlugin>()
+                .AddSingleton<IRelationalTypeMappingSourcePlugin, LocalTimeTypeMappingSourcePlugin>()
+                .AddSingleton<IRelationalTypeMappingSourcePlugin, DurationTypeMappingSourcePlugin>()
+                .AddSingleton<IRelationalTypeMappingSourcePlugin, LocalDateTimeTypeMappingSourcePlugin>()
+                .AddSingleton<IRelationalTypeMappingSourcePlugin, OffsetDateTimeTypeMappingSourcePlugin>()
+                .AddSingleton<IProviderCodeGeneratorPlugin, SqlServerNodaTimeCodeGeneratorPlugin>();
+    }
+}

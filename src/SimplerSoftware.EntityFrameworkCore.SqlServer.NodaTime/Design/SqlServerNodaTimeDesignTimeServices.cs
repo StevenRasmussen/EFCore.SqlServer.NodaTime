a936f29179efd44d36d5285fe1e454c87b764d38
Steven Rasmussen <steven.rasmussen@gmail.com>
2021-09-21 18:45:09 -0600

    Remove workaround since fix is now in place.

--- src/SimplerSoftware.EntityFrameworkCore.SqlServer.NodaTime/Design/SqlServerNodaTimeDesignTimeServices.cs
+++ src/SimplerSoftware.EntityFrameworkCore.SqlServer.NodaTime/Design/SqlServerNodaTimeDesignTimeServices.cs
@@ -14,19 +14,6 @@ namespace SimplerSoftware.EntityFrameworkCore.SqlServer.NodaTime.Design
     {
         public void ConfigureDesignTimeServices(IServiceCollection serviceCollection)
         {
-            // We are replacing the default implementation of the 'SqlServerTypeMappingSource' due to a bug. See here: https://github.com/dotnet/efcore/issues/24660
-            var insertIndex = serviceCollection.Count - 1;
-#pragma warning disable EF1001 // Internal EF Core API usage.
-            var sqlServerTypeMappingSource = serviceCollection.FirstOrDefault(x => x.ServiceType == typeof(IRelationalTypeMappingSource) && x.ImplementationType == typeof(SqlServerTypeMappingSource));
-#pragma warning restore EF1001 // Internal EF Core API usage.
-            if (sqlServerTypeMappingSource != null)
-            {
-                insertIndex = serviceCollection.IndexOf(sqlServerTypeMappingSource);
-                serviceCollection.Remove(sqlServerTypeMappingSource);
-            }
-
-            serviceCollection.Insert(insertIndex, new ServiceDescriptor(typeof(IRelationalTypeMappingSource), typeof(SqlServerNodaTimeTypeMappingSource), ServiceLifetime.Singleton));
-
             serviceCollection
                 .AddSingleton<IRelationalTypeMappingSourcePlugin, SqlServerNodaTimeTypeMappingSourcePlugin>()
                 .AddSingleton<IProviderCodeGeneratorPlugin, SqlServerNodaTimeCodeGeneratorPlugin>();

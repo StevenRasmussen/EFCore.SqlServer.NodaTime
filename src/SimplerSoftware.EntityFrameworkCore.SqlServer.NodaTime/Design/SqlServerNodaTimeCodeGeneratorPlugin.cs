0e73a3f55f5811d8541dab72032ca3f38c91575e
Steven Rasmussen <steven.rasmussen@gmail.com>
2021-09-21 18:45:34 -0600

    Update deprecated code.

--- src/SimplerSoftware.EntityFrameworkCore.SqlServer.NodaTime/Design/SqlServerNodaTimeCodeGeneratorPlugin.cs
+++ src/SimplerSoftware.EntityFrameworkCore.SqlServer.NodaTime/Design/SqlServerNodaTimeCodeGeneratorPlugin.cs
@@ -1,12 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.EntityFrameworkCore.Scaffolding;
+using System.Reflection;
 
 namespace SimplerSoftware.EntityFrameworkCore.SqlServer.NodaTime.Design
 {
     public class SqlServerNodaTimeCodeGeneratorPlugin : ProviderCodeGeneratorPlugin
     {
-        public override MethodCallCodeFragment GenerateProviderOptions() 
-            => new MethodCallCodeFragment(nameof(SqlServerDbContextOptionsBuilderExtensions.UseNodaTime));
+        public override MethodCallCodeFragment GenerateProviderOptions()
+        {
+            var methodInfo = typeof(SqlServerDbContextOptionsBuilderExtensions).GetMethod(nameof(SqlServerDbContextOptionsBuilderExtensions.UseNodaTime), BindingFlags.Public | BindingFlags.Static);
+            return new MethodCallCodeFragment(methodInfo);
+        }
     }
 }

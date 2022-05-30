9617b62b189eef38ff23fafbbe202d75954cb695
Steven Rasmussen <steven.rasmussen@gmail.com>
2022-05-30 09:47:18 -0600

    Fixed an issue with the 'ServiceCollectionTests'

--- src/SimplerSoftware.EntityFrameworkCore.SqlServer.NodaTime.Tests/ServiceCollectionTests.cs
+++ src/SimplerSoftware.EntityFrameworkCore.SqlServer.NodaTime.Tests/ServiceCollectionTests.cs
@@ -1,38 +1,38 @@
-using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using SimplerSoftware.EntityFrameworkCore.SqlServer.NodaTime.Tests.Models;
 using Xunit;
 
-namespace SimplerSoftware.EntityFrameworkCore.SqlServer.NodaTime.Tests;
-
-public class ServiceCollectionTests
+namespace SimplerSoftware.EntityFrameworkCore.SqlServer.NodaTime.Tests
 {
-    [Fact]
-    public void CustomServiceCollection_ShouldNotThrowError()
+    public class ServiceCollectionTests
     {
-        var serviceProvider = new ServiceCollection()
-            .AddEntityFrameworkSqlServer()
-            .AddNodaTime()
-            .BuildServiceProvider(true);
-
-        var builder = new DbContextOptionsBuilder<SimpleContext>()
-            .UseInternalServiceProvider(serviceProvider)
-            .UseSqlServer("Does not matter since we won't try to connect to the DB", opt => { opt.UseNodaTime(); });
-
-        using var dbContext = new SimpleContext(builder.Options);
-
-        dbContext.Add(new SupportedNodaTypes());
-        Assert.Equal(1, dbContext.ChangeTracker.Entries().Count());
-    }
-
-    private class SimpleContext : DbContext
-    {
-        public SimpleContext(DbContextOptions options)
-            : base(options)
+        [Fact]
+        public void CustomServiceCollection_ShouldNotThrowError()
         {
+            var serviceProvider = new ServiceCollection()
+                .AddEntityFrameworkSqlServer()
+                .AddNodaTime()
+                .BuildServiceProvider(true);
+
+            var builder = new DbContextOptionsBuilder<SimpleContext>()
+                .UseInternalServiceProvider(serviceProvider)
+                .UseSqlServer("Does not matter since we won't try to connect to the DB", opt => { opt.UseNodaTime(); });
+
+            using var dbContext = new SimpleContext(builder.Options);
+
+            dbContext.Add(new SupportedNodaTypes());
+            Assert.Single(dbContext.ChangeTracker.Entries());
         }
 
-        public DbSet<SupportedNodaTypes> TypeResults { get; set; }
+        private class SimpleContext : DbContext
+        {
+            public SimpleContext(DbContextOptions options)
+                : base(options)
+            {
+            }
+
+            public DbSet<SupportedNodaTypes> TypeResults { get; set; }
+        }
     }
 }

50a268790abffb470922198020470714b158babf
Steve Rasmussen <srasmussen@prenticeworx.com>
2023-10-10 17:42:21 -0600

    Fixed breaking changes with RC2.

--- src/SimplerSoftware.EntityFrameworkCore.SqlServer.NodaTime/Storage/SqlServerNodaTimeTypeMappingSourcePlugin.cs
+++ src/SimplerSoftware.EntityFrameworkCore.SqlServer.NodaTime/Storage/SqlServerNodaTimeTypeMappingSourcePlugin.cs
@@ -50,7 +50,7 @@ namespace SimplerSoftware.EntityFrameworkCore.SqlServer.NodaTime.Storage
             this.AddTypeMappingToDictionaries(this._durationTypeMapping);
         }
 
-        public virtual RelationalTypeMapping FindMapping(in RelationalTypeMappingInfo mappingInfo)
+        public RelationalTypeMapping FindMapping(in RelationalTypeMappingInfo mappingInfo)
         {
             var clrType = mappingInfo.ClrType;
             var storeTypeName = mappingInfo.StoreTypeName;
@@ -73,11 +73,11 @@ namespace SimplerSoftware.EntityFrameworkCore.SqlServer.NodaTime.Storage
                 if (StoreTypeMappings.TryGetValue(storeTypeNameBase!, out mappings))
                 {
                     if (clrType == null)
-                        return mappings[0].Clone(in mappingInfo);
+                        return mappings[0].Clone(mappingInfo);
 
                     foreach (var m in mappings)
                         if (m.ClrType == clrType)
-                            return m.Clone(in mappingInfo);
+                            return m.Clone(mappingInfo);
 
                     return null;
                 }
@@ -88,7 +88,7 @@ namespace SimplerSoftware.EntityFrameworkCore.SqlServer.NodaTime.Storage
 
             // TODO: Cache size/precision/scale mappings?
             return mappingInfo.Precision.HasValue && _hasPrecisionTypes.Contains(mapping.ClrType)
-                ? mapping.Clone($"{mapping.StoreType}({mappingInfo.Precision.Value})", null)
+                ? mapping.Clone(mappingInfo)
                 : mapping;
         }
 

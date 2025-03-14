09971b35439760fe6a23eb9b7cdb12382de98f28
Steven Rasmussen <steven.rasmussen@gmail.com>
2025-03-14 09:24:35 -0600

    Added the 'AzureSqlDbContextOptionsBuilderExtensions' extension class to support Azure SQL Db's.

---
+++ src/SimplerSoftware.EntityFrameworkCore.SqlServer.NodaTime/Extensions/AzureSqlDbContextOptionsBuilderExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.SqlServer.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microsoft.EntityFrameworkCore;
+
+public static class AzureSqlDbContextOptionsBuilderExtensions
+{
+    public static AzureSqlDbContextOptionsBuilder UseNodaTime(
+        this AzureSqlDbContextOptionsBuilder azureBuilder)
+    {
+        // Access the underlying OptionsBuilder
+        var coreOptionsBuilder = ((IRelationalDbContextOptionsBuilderInfrastructure)azureBuilder).OptionsBuilder;
+
+        // Look for the NodaTime extension or create a new instance if not found
+        var extension = coreOptionsBuilder.Options.FindExtension<NodaTimeOptionsExtension>()
+            ?? new NodaTimeOptionsExtension();
+
+        // Add or update the extension in the options
+        ((IDbContextOptionsBuilderInfrastructure)coreOptionsBuilder).AddOrUpdateExtension(extension);
+
+        return azureBuilder;
+    }
+}
